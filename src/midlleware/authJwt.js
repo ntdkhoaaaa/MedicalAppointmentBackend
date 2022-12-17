a4498e5180167991dd2cb7e6bd7595471d6b8fcd
Phuc Doan <phucdpb@gmail.com>
2022-12-17 13:01:20 +0700

    jwt

--- src/midlleware/authJwt.js
+++ src/midlleware/authJwt.js
@@ -4,14 +4,14 @@ import db from "../models/index";
 
 let verifyToken = (req, res, next) => {
     try {
-        let token = req.headers.authorization || req.body.token || req.query.token;
-        console.log('token', token, typeof (token))
-        if (!token) {
+        let authHeader = req.headers.authorization || req.body.token || req.query.token;
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {
             return res.status(200).json({
                 errCode: 2,
                 role: 'R0'
             });
         }
+        let token = authHeader.substring(7, authHeader.length);
         jwt.verify(token, process.env.JSON_SECRET_ACCESS, (err, decoded) => {
             if (err) {
                 return res.status(200).json({
@@ -35,14 +35,14 @@ let verifyToken = (req, res, next) => {
 };
 let checkPermissionByToken = async (req, res) => {
     try {
-        let token = req.headers.authorization || req.body.token || req.query.token;
-        console.log('token', token, typeof (token))
-        if (!token) {
+        let authHeader = req.headers.authorization || req.body.token || req.query.token;
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {
             return res.status(200).json({
                 errCode: 2,
                 role: 'R0'
             });
         }
+        let token = authHeader.substring(7, authHeader.length);
         jwt.verify(token, process.env.JSON_SECRET_ACCESS, async (err, decoded) => {
             if (err) {
                 return res.status(200).json({
@@ -69,36 +69,6 @@ let checkPermissionByToken = async (req, res) => {
             errCode: -1,
             errMessage: 'Error from server',
 
-        })
-
-    }
-};
-let refreshToken = (req, res, next) => {
-    try {
-        let token = req.headers.authorization || req.body.token || req.query.token;
-        console.log('token', token, typeof (token))
-        if (!token) {
-            return res.status(200).json({
-                errCode: 2,
-                role: 'R0'
-            });
-        }
-        jwt.verify(token, process.env.JSON_SECRET, (err, decoded) => {
-            if (err) {
-                return res.status(200).json({
-                    errCode: 3,
-                    role: 'R0'
-                });
-            }
-            req.body.authId = decoded.id;
-            req.body.authRole = decoded.roleId;
-            next();
-        });
-
-    } catch (error) {
-        return res.status(200).json({
-            errCode: -1,
-            errMessage: 'Error from server'
         })
 
     }

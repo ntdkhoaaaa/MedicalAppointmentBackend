919de61e36b8930b8c44c97266da47adcf415f98
Phuc Doan <phucdpb@gmail.com>
2022-12-18 23:36:04 +0700

    aa

--- src/server.js
+++ src/server.js
@@ -33,8 +33,8 @@ app.use(function (req, res, next) {
 });
 
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }))
 
 
 app.use(bodyParser.json({ limit: '50mb' }));

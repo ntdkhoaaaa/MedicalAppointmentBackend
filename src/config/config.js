f36dd70d292c4fa6928c0168924ef9ad84a5f964
ntdkhoaaaa <19154035@student.hcmute.edu.vn>
2023-05-31 21:50:12 +0700

    upppp

--- src/config/config.js
+++ src/config/config.js
@@ -26,41 +26,13 @@ module.exports = {
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE_NAME,
     "host": process.env.DB_HOST,
-    "port": process.env.DB_PORT,
     "dialect": process.env.DB_DIALECT,
-    "define": {
-      "freezeTableName": true
-    },
-    dialectOptions:
-      process.env.DB_SSL === 'true' ?
-        {
-          ssl: {
-            require: true,
-            rejectUnauthorized: false
-          }
-        } : {}
-    ,
-    "timezone": "+07:00"
   },
   "production": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE_NAME,
     "host": process.env.DB_HOST,
-    "port": process.env.DB_PORT,
     "dialect": process.env.DB_DIALECT,
-    "define": {
-      "freezeTableName": true
-    },
-    dialectOptions:
-      process.env.DB_SSL === 'true' ?
-        {
-          ssl: {
-            require: true,
-            rejectUnauthorized: false
-          }
-        } : {}
-    ,
-    "timezone": "+07:00"
   }
 };
